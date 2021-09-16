34a25a76295a28972975219153cd96893e5158bd
victorfeagins <67335904+victorfeagins@users.noreply.github.com>
2021-09-15 19:38:45 -0500

    Made map of charter shchools in 2019 - 2020. We have less charter schools in rural areas

--- ExploringData.R
+++ ExploringData.R
@@ -1,8 +1,10 @@
 # Packages ----
 library(dplyr)
 library(stringr)
-library(ggplot2)
 
+library(sf)
+library(ggplot2)
+library(mapview)
 
 df <- read.csv("Data/2021_2015_Char.csv", 
                header = TRUE,
@@ -13,7 +15,30 @@ df.clean <- df %>%
   mutate(across(everything(), ~str_trim(str_replace(.,"â€", "NA")))) # Replace special characters with NA
 
 # Plotting Charter vs Public ----
+## Getting Data Spatial ----
+df.spatial <- df.clean %>% 
+  select(School.Name, School.ID...NCES.Assigned..Public.School..Latest.available.year,
+         Charter.School..Public.School..2019.20, Longitude..Public.School..2019.20, Latitude..Public.School..2019.20) %>%
+  mutate(Longitude..Public.School..2019.20=as.numeric(Longitude..Public.School..2019.20)) %>% 
+  filter(!is.na(Longitude..Public.School..2019.20)) %>% 
+  st_as_sf(coords = c("Longitude..Public.School..2019.20", "Latitude..Public.School..2019.20"))
+  
+## Getting Texas County ----
+library(tigris)
+TX.county <- counties(state = "TX")
+#st_crs(TX.county) <- 4269
 
+## Trying ggplot ----
+ggplot(df.spatial) +
+  geom_sf(mapping = aes(col = Charter.School..Public.School..2019.20)) +
+  xlab("Longitude") + ylab("Latitude")
+  
 
+## Trying mapview ----
+
+mapview(TX.county) +
+  mapview(df.spatial,
+          zcol = "Charter.School..Public.School..2019.20",
+          map.types = "OpenStreetMap")
 
 
