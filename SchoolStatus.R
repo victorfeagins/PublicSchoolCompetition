bdcf59e6b63f02d7436bcd3e283e40d1d921297a
victorfeagins <67335904+victorfeagins@users.noreply.github.com>
2021-09-20 11:04:54 -0500

    Need to figure out a way to find a way to label obs the year they opened.

--- SchoolStatus.R
+++ SchoolStatus.R
@@ -4,6 +4,7 @@
 
 #Packages ----
 library(dplyr)#For dataframe manipulation
+library(tidyr) #For transposing data
 library(purrr) # For effective interation
 library(stringr) #For handling of strings
 
@@ -11,14 +12,14 @@ library(stringr) #For handling of strings
 filevector <- c("Data/Status1989_1986.csv", "Data/Status1999_1900.csv", "Data/Status2009_2000.csv",
                 "Data/Status2019_2010.csv")
 
-
-#Reading in csv's ----
+# Data Processing ----
+## Reading in csv's ----
 df <- lapply(filevector, read.csv, header = TRUE, skip = 6) %>%  #Read in all the csv's
   lapply(filter, row_number() <= n()-4) %>% # In each csv file remove footer of 4 rows
   reduce(full_join)  #Merge them all by common variable names (name, state, ID)
 
 
-#Renaming Column names ----
+## Renaming Column names ----
 
 RenameVar <- function(title, string){
   str_c(title, str_sub(string, -7)) #Combines the title and the last 7 characters (year)
@@ -29,9 +30,16 @@ df.order <- df %>%
   select(sort(names(.))) #Sorts variables alphabetically
 
 
-#Replacing special characters
+## Replacing special characters ----
 
 df.order <- df.order %>% 
-  mutate(across(everything(), ~str_trim(str_replace(.,"â€", "NA")))) %>%  # Replace special characters with NA
-  na_if("NA")
+  mutate(across(everything(), ~str_trim(str_replace(.,"â€", "NA")))) %>%  # Replace special characters with "NA"
+  na_if("NA") #making "NA" into real NA
 
+
+## Transposing Data ----
+df.trans <- df.order %>% 
+  pivot_longer(starts_with("Status"))
+
+df.trans.nm <- df.trans %>% 
+  filter(!is.na(value))
