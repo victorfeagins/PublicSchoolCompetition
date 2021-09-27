f09e171b8aaac6d92d0dfb802cb5d3f056ac5cdd
victorfeagins <67335904+victorfeagins@users.noreply.github.com>
2021-09-27 09:56:45 -0500

    Set Up

---
+++ Defining_Sample.R
@@ -0,0 +1,10 @@
+#The script is exploring the various observations in the Common Core
+#Data set to find the ID's that are really IDs
+
+#Packages ----
+library(dplyr)#For dataframe manipulation
+library(tidyr) #For transposing data
+library(purrr) # For effective iteration
+library(stringr) #For handling of strings
+
+
