636afeedebbc68b4888b0f4084ddc978225c422f
victorfeagins <67335904+victorfeagins@users.noreply.github.com>
2021-12-03 10:45:10 -0600

    Subset by schools active for 10 years or more

--- Defining_Sample.R
+++ Defining_Sample.R
@@ -1,5 +1,4 @@
-#The script is exploring the various observations in the Common Core
-#Data set to find the ID's that are really IDs
+#The script is defining the sample of our analysis
 
 # Packages ----
 library(dplyr)#For dataframe manipulation
@@ -19,83 +18,43 @@ df <- df %>%
 
 
 
-# Looking at schools that have existed for more then 1 year ----
+# Calculating Years Active ----
 
-test <- df %>% 
+df <- df %>% 
   rowwise() %>% 
-  mutate(YearsActive = sum(str_count(c_across(starts_with("Start.of.Year")), "1|3"), na.rm = TRUE))
+  mutate(YearsActive = sum(str_count(c_across(starts_with("Start.of.Year")), "1|3"), na.rm = TRUE)) %>%  #Counting 1 open and 3 new schools
+  ungroup() #removes grouping from rowwise
+
+hist(df$YearsActive) # Most schools have the all the years open
   
+table(df$YearsActive)
+
+#It might be possible to actually get the ranges of 
+
+
+
+# Finding the time frame the school is open or at least reported data -----
+
+df <- df %>% 
+  mutate(YearsReported = str_split(Years.School.Reported.Data..Public.School..Latest.available.year, " |-")) %>% 
+  rowwise() %>% #Rowwise is nice for list columns
+  mutate(FirstYear = min(as.numeric(YearsReported)),
+         LastYear = max(as.numeric(YearsReported))) %>% 
+  ungroup()
+
+
+
+# Filtering schools 10 years or greater ----
+#10 chosen out of simplicity
+
+df.subset <- df %>%
+  filter(YearsActive >= 10)
+
+
+
+
+
+
+
+
   
-
-
-
-#Problems of the same schools having multiple IDs in the dataset
-
-sum(duplicated(df)) # No duplicates including ID's 
-
-df %>% 
-  select(-School.ID...NCES.Assigned..Public.School..Latest.available.year, 
-         -Agency.ID...NCES.Assigned..Public.School..Latest.available.year) %>% 
-  duplicated() %>% 
-  sum()
-#Without IDs 497 schools have the same information.
-
-
-### Let's add new information and see if the duplicated changes
-
-## Adding teacher and student numbers ----
-df.t <- read.csv("Data2/Students_Teachers_2019_1986.csv", 
-                 header = TRUE,
-                 skip = 6) %>% 
-  head(-4)
-
-df.t <- df.t %>% 
-  mutate(across(everything(), ~ifelse(str_detect(.,pattern = "â€"), NA, .)))
-
-df.merged <- df %>% 
-  left_join(df.t)
-
-
-df.merged %>% 
-  select(-School.ID...NCES.Assigned..Public.School..Latest.available.year, 
-         -Agency.ID...NCES.Assigned..Public.School..Latest.available.year) %>% 
-  duplicated() %>% 
-  sum()
-#Dang there are less duplication that means that adding student and teacher information is different across duplication
-
-## Examining duplications by eye
-
-old.duplicated<- df %>% 
-  select(-School.ID...NCES.Assigned..Public.School..Latest.available.year, 
-         -Agency.ID...NCES.Assigned..Public.School..Latest.available.year) %>% 
-  duplicated()
-
-test <- df.merged %>% 
-  filter(old.duplicated) %>% 
-  select(names(df.t))
-
-
-## Adding Spatial information ----
-df.spat <- read.csv("Data2/Long_Lat_2019_2000.csv", 
-                 header = TRUE,
-                 skip = 6) %>% 
-  head(-4)
-
-
-df.merged.2 <- df.merged %>% 
-  left_join(df.spat)
-
-
-df.merged.2 %>% 
-  select(-School.ID...NCES.Assigned..Public.School..Latest.available.year, 
-         -Agency.ID...NCES.Assigned..Public.School..Latest.available.year) %>% 
-  duplicated() %>% 
-  sum()
-
-dup <- df.merged.2 %>% 
-  select(-School.ID...NCES.Assigned..Public.School..Latest.available.year, 
-         -Agency.ID...NCES.Assigned..Public.School..Latest.available.year) %>% 
-  duplicated()
-test <- df.merged.2 %>% 
-  filter(dup) %>% 
-  select(names(df.spat))
